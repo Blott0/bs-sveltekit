3e0424439164cce2f02333fa721fa73bb850213b
Rik Meijer <rikmeijer442@gmail.com>
2022-01-27 19:58:54 +0100

    removed comment

--- src/lib/_db.js
+++ src/lib/_db.js
@@ -2,14 +2,6 @@ const db = {
     "host": 'mongodb://127.0.0.1:27017',
     "db": "BlottBase",
     "collection": "plays"
-};
-
-// const db = {
-//     "host": "blottsite.dixhi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-//     "user": "BlottSite",
-//     "password": "Yog-Sothoth",
-//     "db": "BlottBase",
-//     "collection": "games"
-// };
+}
 
 export default db;
