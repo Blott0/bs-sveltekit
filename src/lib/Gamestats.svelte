4c00efb4b6cf009237f7c823beb6f2982c86d104
Rik Meijer <rikmeijer442@gmail.com>
2022-01-27 18:57:56 +0100

    added highscores and most wins

--- src/lib/Gamestats.svelte
+++ src/lib/Gamestats.svelte
@@ -1,30 +1,83 @@
 <script>
 
-    export let stats;
+    export let stats
 
-    import { slide } from 'svelte/transition';
+    import { slide } from 'svelte/transition'
 
-    let sortedStats = sortbygame(stats);
-    let gamestats = sortedStats.sorted;
-    let nameLookupTable = sortedStats.lookup;
-    let itemsExpanded = [];
-    let playsExpanded = [];
+    let sortedStats = sortbygame(stats)
+    let gamestats = sortedStats.sorted
+    let nameLookupTable = sortedStats.lookup
+    const highscores = gethighscores(gamestats)
+    const gamedominance = getdominance(gamestats)
+    let itemsExpanded = []
+    let playsExpanded = []
+
+    function gethighscores(stats) {
+        let highscores = {}
+        Object.keys(nameLookupTable).forEach(gameid => {
+            highscores[gameid] = { result: { points: 0 } }
+            stats[gameid].forEach(result => {
+                result.result.forEach(res => {
+                    if (res.result.points && (res.result.points > highscores[gameid].result.points)) {
+                        highscores[gameid] = res
+                    }
+                });
+                
+            })
+        })
+        return highscores
+    }
+
+    function getdominance(stats) {
+        let dominance = {}
+        Object.keys(nameLookupTable).forEach(gameid => {
+            dominance[gameid] = {}
+            stats[gameid].forEach(stat => {
+                stat.result.forEach(playerresult => {
+                    if (playerresult.result.win) {
+
+                        if (!dominance[gameid][playerresult.player.name]) {
+                            dominance[gameid][playerresult.player.name] = 1
+                        }
+                        else {
+                            dominance[gameid][playerresult.player.name] ++
+                        }
+                    }
+                })
+            })
+            let highest = 0
+            Object.keys(dominance[gameid]).forEach(name => {
+                if (dominance[gameid][name] > highest) {
+                    highest = dominance[gameid][name]
+                }
+            })
+            Object.keys(dominance[gameid]).forEach(name => {
+                if (dominance[gameid][name] < highest) {
+                    delete dominance[gameid][name]
+                }
+            })
+            if (!Object.keys(dominance[gameid]).length) {
+                delete dominance[gameid]
+            }
+        })
+        return dominance
+    }
 
     function sortbygame(s) {
         
-        let sorted = {};
-        let lookup = {};
+        let sorted = {}
+        let lookup = {}
         s.forEach(r => {
             
             if (!sorted[r.game._id]) {
-                sorted[r.game._id] = [];
-                lookup[r.game._id] = r.game.name;
+                sorted[r.game._id] = []
+                lookup[r.game._id] = r.game.name
             }
-            sorted[r.game._id].push(r);
+            sorted[r.game._id].push(r)
 
         });
         
-        return { sorted, lookup };
+        return { sorted, lookup }
 
     };
 
@@ -32,15 +85,15 @@
         if (itemsExpanded.includes(id)) {
             for (let i = 0; i < itemsExpanded.length; i++) {
                 if (itemsExpanded[i] === id) {
-                    itemsExpanded.splice(i, 1);
-                    itemsExpanded = itemsExpanded;
-                    return;
+                    itemsExpanded.splice(i, 1)
+                    itemsExpanded = itemsExpanded
+                    return
                 }
             }
         }
         else {
-            itemsExpanded.push(id);
-            itemsExpanded = itemsExpanded;
+            itemsExpanded.push(id)
+            itemsExpanded = itemsExpanded
         }
     }
 
@@ -48,26 +101,26 @@
         if (playsExpanded.includes(id)) {
             for (let i = 0; i < playsExpanded.length; i++) {
                 if (playsExpanded[i] === id) {
-                    playsExpanded.splice(i, 1);
-                    playsExpanded = playsExpanded;
-                    return;
+                    playsExpanded.splice(i, 1)
+                    playsExpanded = playsExpanded
+                    return
                 }
             }
         }
         else {
-            playsExpanded.push(id);
-            playsExpanded = playsExpanded;
+            playsExpanded.push(id)
+            playsExpanded = playsExpanded
         }
     }
 
 </script>
 
-<h1>all plays logged:</h1>
+<h2>all plays logged:</h2>
 
 <ul>
     { #each Object.keys(gamestats) as gameId }
         <li on:click='{e => toggleExpand(gameId)}'>
-            {@html nameLookupTable[gameId] } ({ gamestats[gameId].length })
+            {@html nameLookupTable[gameId]} ({ gamestats[gameId].length })
         </li>
 
         {#if itemsExpanded.includes(gameId)}
@@ -92,6 +145,33 @@
         {/if}
 
     { /each }
+</ul>
+
+<h2>highscores</h2>
+<ul>
+    {#each Object.keys(nameLookupTable) as id}
+        {#if highscores[id].result.points}
+            <li>
+                {nameLookupTable[id]}: {highscores[id].result.points} {highscores[id].player.name}
+            </li>
+        {/if}
+    {/each}
+</ul>
+
+<h2>most wins</h2>
+<ul>
+    {#each Object.keys(gamedominance) as gameid}
+        <li>
+            {nameLookupTable[gameid]}:
+            {#each Object.keys(gamedominance[gameid]) as winner, i}
+                {#if i > 0}
+                , 
+                {/if}
+                {winner} {gamedominance[gameid][winner]}
+            {/each}
+            
+        </li>
+    {/each}
 </ul>
 
 
