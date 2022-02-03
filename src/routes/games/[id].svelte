19f10c1c7e8f037217ffeedb2834630b8e75378f
Rik Meijer <rikmeijer442@gmail.com>
2022-02-03 02:36:33 +0100

    added section elements

--- src/routes/games/[id].svelte
+++ src/routes/games/[id].svelte
@@ -53,57 +53,72 @@
     
 </script>
 
-<img src="{game.image}">
+<section>
 
-<h2>{game.name}</h2>
+    <img src="{game.image}">
 
-<p>{game.boardgamepublisher}</p>
+    <h2>{game.name}</h2>
 
-<div class="players" title="{game.minplayers.length} - {game.minplayers.length + game.maxplayers.length} players">
-    {#each game.minplayers as player}
-        {@html fig}
-    {/each}
-    {#each game.maxplayers as player}
-        {@html fig2}
-    {/each}
-</div>
+    <p>{game.boardgamepublisher}</p>
 
-<div class="weight" title="weight: {game.averageweight}">
-    <svg viewBox="0 0 169.9 130.5">
-        <path fill="hsl({(120 - game.averageweight / 5 * 120)},100%,50%)" fill-rule="evenodd" d="M78.1 0a66.2 40.6 0 0 0-66 38.3A41.4 28.3 0 0 0 0 58.3a41.4 28.3 0 0 0 41.4 28.3 41.4 28.3 0 0 0 10.4-1 34 22.9 0 0 0 29.6 11.7 34 22.9 0 0 0 .9 0 61.2 61.2 0 0 0 5 33.2l22-1s-2.4-8.3-2.5-19.5a35 21.9 25.5 0 0 35.7-8.9 35 21.9 25.5 0 0 .4-7.4 32 31.6 0 0 0 27-31.2 32 31.6 0 0 0-27.5-31.2A66.2 40.6 0 0 0 78.1 0z"/>
-    </svg>
-</div>
+    <div class="players" title="{game.minplayers.length} - {game.minplayers.length + game.maxplayers.length} players">
+        {#each game.minplayers as player}
+            {@html fig}
+        {/each}
+        {#each game.maxplayers as player}
+            {@html fig2}
+        {/each}
+    </div>
 
-<ul>
-    {#each game.boardgamedesigner as des}
-        <li>
-            {des}
-        </li>
-    {/each}
-</ul>
+    <div class="weight" title="weight: {game.averageweight}">
+        <svg viewBox="0 0 169.9 130.5">
+            <path fill="hsl({(120 - game.averageweight / 5 * 120)},100%,50%)" fill-rule="evenodd" d="M78.1 0a66.2 40.6 0 0 0-66 38.3A41.4 28.3 0 0 0 0 58.3a41.4 28.3 0 0 0 41.4 28.3 41.4 28.3 0 0 0 10.4-1 34 22.9 0 0 0 29.6 11.7 34 22.9 0 0 0 .9 0 61.2 61.2 0 0 0 5 33.2l22-1s-2.4-8.3-2.5-19.5a35 21.9 25.5 0 0 35.7-8.9 35 21.9 25.5 0 0 .4-7.4 32 31.6 0 0 0 27-31.2 32 31.6 0 0 0-27.5-31.2A66.2 40.6 0 0 0 78.1 0z"/>
+        </svg>
+        <span>{game.averageweight}</span>
+    </div>
 
-<ul>
-    {#each game.boardgamecategory as cat}
-        <li>
-            {cat}
-        </li>
-    {/each}
-</ul>
+    <ul>
+        {#each game.boardgamedesigner as des}
+            <li>
+                {des}
+            </li>
+        {/each}
+    </ul>
 
-<ul>
-    {#each game.boardgamemechanic as mec}
-        <li>
-            {mec}
-        </li>
-    {/each}
-</ul>
+    <ul>
+        {#each game.boardgamecategory as cat}
+            <li>
+                {cat}
+            </li>
+        {/each}
+    </ul>
 
-<p>{@html unesc(game.description)}</p>
+    <ul>
+        {#each game.boardgamemechanic as mec}
+            <li>
+                {mec}
+            </li>
+        {/each}
+    </ul>
+
+    <p>{@html unesc(game.description)}</p>
+
+</section>
 
 <style>
 
     img {
         max-width: 100%;
     }
+
+    .weight {
+        display: flex;
+        align-items: center;
+    }
+
+    .weight > span {
+        margin-left: 4px;
+    }
+
 
 </style>
--- src/routes/stats/[id].svelte
+++ src/routes/stats/[id].svelte
@@ -14,13 +14,15 @@
     export let play
 </script>
 
-<h2>{play.game.name}</h2>
-<h3>{play.date}</h3>
-<div class="players">
-    {#each play.result as res}
-        {res.player.name}
-        {res.result.points}
-        {res.result.win ? 'won' : ''}
-        <br>
-    {/each}
-</div>
+<section>
+    <h2>{play.game.name}</h2>
+    <h3>{play.date}</h3>
+    <div class="players">
+        {#each play.result as res}
+            {res.player.name}
+            {res.result.points}
+            {res.result.win ? 'won' : ''}
+            <br>
+        {/each}
+    </div>
+</section>
