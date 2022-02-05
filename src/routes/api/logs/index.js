84592ae7665fd946e00da61c1c5897ab02f0ad13
Rik Meijer <rikmeijer442@gmail.com>
2022-02-05 16:30:20 +0100

    increased number of logs

--- src/routes/api/logs/index.js
+++ src/routes/api/logs/index.js
@@ -5,8 +5,7 @@ export async function get(request) {
     const db = dbConnection.db("BlottBase")
     const logscollection = db.collection('logs')
 
-    const log = await logscollection.find({}).sort({date: -1}).limit(3).toArray()
-    console.log(log)
+    const log = await logscollection.find({}).sort({date: -1}).limit(5).toArray()
     return {
         status: 200,
         body: log
